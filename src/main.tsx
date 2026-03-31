69f0b5e4b1b7759d06c32a60c95daf91a5b2b26e
Imacul <imacul77@gmail.com>
2026-03-31 07:48:51 +0100

    Enable Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
+import { Analytics } from '@vercel/analytics/react';
 
 import App from './App';
 import { AuthProvider } from './features/auth/AuthProvider';
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root') as HTMLElement).render(
     <AuthProvider>
       <BrowserRouter>
         <App />
+        <Analytics />
       </BrowserRouter>
     </AuthProvider>
   </React.StrictMode>
